432068793a151043aa3533a5af4b9269ace5bc57
Deborah M Ingabire <ingabineza@gmail.com>
2020-10-11 15:35:13 +0200

    added a total price functionality

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
-var totalPrice = "";
-
 $(function(){
-    $("#form-order").submit(function(event){
+    $("#see-order").click(function(event){
         event.preventDefault();
+
+         // calculating the total price.
+
+        var chargesOfPizzaSize = parseInt($("#size-charges").val());
+        if(isNaN(chargesOfPizzaSize)){
+            chargesOfPizzaSize = 0;
+        }
+        var chargesOfCrust = parseInt($("#crust-charges").val());
+        if(isNaN(chargesOfCrust)){
+            chargesOfCrust = 0;
+        }
+        var chargesOfToppings = parseInt($("#toppings-charges").val());
+        if(isNaN(chargesOfToppings)){
+            chargesOfToppings = 0;
+        }
         
-    })
+        var temp = chargesOfPizzaSize + chargesOfCrust + chargesOfToppings;
+
+        var newPrice = new Orderprice(chargesOfPizzaSize,chargesOfCrust,chargesOfToppings);
+        newPrice.output(temp);
+
+        /////////////////////////////////////////
+
+    });
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function Order(pizzaSize, sizeCharges, crust, crustCharges, toppings, toppingsCharges, pizzaType, pizzaNumber){
-    this.pizzaSize = pizzaSize;
-    this.sizeCharges = sizeCharges;
-    this.crust = crust;
-    this.crustCharges = crustCharges;
-    this.toppings = toppings;
-    this.toppingsCharges = toppingsCharges;
-    this.pizzaType = pizzaType;
-    this.pizzaSize = pizzaSize;
+// constructor and prototypes of total price calculations
+function Orderprice( sizeCharges, crustCharges,toppingsCharges){
+    
+    this.size = sizeCharges;
+    this.crust = crustCharges;
+    this.toppings= toppingsCharges;
 }
+Orderprice.prototype.output = function(temp)
+{
+   $(".total-price").text( "RWF " + temp);
+
+};
+////////////////////////////////////////////////////
